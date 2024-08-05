e7a7c6042431737a99edac9bfb4bd45eed53a0b4
f77433es <eduard.sarkisyan@student.manchester.ac.uk>
2024-08-05 17:21:47 +0300

    Set up EJS layout, default CSS and per-page CSS

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
+const expressLayouts = require('express-ejs-layouts');
 
 const indexRouter = require("./routes/indexRouter");
 const developersRouter = require("./routes/developersRouter");
@@ -8,7 +9,15 @@ const gamesRouter = require("./routes/gamesRouter");
 const genresRouter = require("./routes/genresRouter");
 
 app.set("view engine", "ejs");
+app.use(expressLayouts);
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware to set the cssFile variable
+app.use((req, res, next) => {
+    res.locals.cssFile = null;
+    next();
+});
 
 app.use("/genres", genresRouter);
 app.use("/games", gamesRouter);
--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 
 exports.indexGet = asyncHandler(async (req, res) => {
-    res.render("index", { title: "Home" });
+    res.render("index", { title: "Home", cssFile: "/css/index.css" });
 });
 
 exports.aboutGet = asyncHandler(async (req, res) => {
